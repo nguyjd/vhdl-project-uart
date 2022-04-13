b07f3ffea0ddde70d4aa2902a973a1d46405acc4
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-12 22:27:23 -0400

    Created two new files for the FIFO buffer

---
+++ vhdl-project-uart.srcs/sources_1/new/fifo_buffer.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04/12/2022 07:58:02 PM
+-- Design Name: 
+-- Module Name: fifo_buffer - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity fifo_buffer is
+--  Port ( );
+end fifo_buffer;
+
+architecture Behavioral of fifo_buffer is
+
+begin
+
+
+end Behavioral;

8b64d9bf705344585af5c468a8787893c74179ce
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-13 15:23:59 -0400

    Removed the delay for reading from REG

--- vhdl-project-uart.srcs/sources_1/new/register_file.vhd
+++ vhdl-project-uart.srcs/sources_1/new/register_file.vhd
@@ -18,30 +18,25 @@ architecture Behavioral of register_file is
     type registers is array(0 to REGISTERS_AMOUNT - 1) of std_logic_vector (D_WIDTH - 1 downto 0);
     signal registerfile : registers := (others => (others => '0'));
     
-    signal read_reg, read_next: std_logic_vector(D_WIDTH - 1 downto 0) := (others => '0');
-    
 begin
 
-    process (clk, reset, read_addr, write_enable, write_addr, write_data) 
+    process (clk, reset, write_enable, write_addr, write_data) 
     begin
         
         -- Normal reset
         if (reset = '1') then
             registerfile <= (others => (others => '0'));
-            read_reg <= (others => '0');
         elsif (clk'event and clk = '1') then
-            read_reg <= read_next;
             
             if (write_enable = '1') then
                 registerfile(TO_INTEGER(unsigned(write_addr))) <= write_data;
             end if;
             
         end if;
-        -- Read the reg at read_addr.
-        read_next <= registerfile(TO_INTEGER(unsigned(read_addr)));
+        
     end process;
     
--- Output Logic
-read_data <= read_reg;
+    -- Output Logic
+    read_data <= registerfile(TO_INTEGER(unsigned(read_addr)));
 
 end Behavioral;

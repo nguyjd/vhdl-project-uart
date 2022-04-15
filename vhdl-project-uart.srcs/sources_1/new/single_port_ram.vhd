0f445d36ad3f079cbbe2531d7d1d070a6349c59d
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-14 22:25:32 -0400

    Created ram file

---
+++ vhdl-project-uart.srcs/sources_1/new/single_port_ram.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+
+entity single_port_ram is
+--  Port ( );
+end single_port_ram;
+
+architecture Behavioral of single_port_ram is
+
+begin
+
+
+end Behavioral;

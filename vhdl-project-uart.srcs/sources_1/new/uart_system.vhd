dab4a1ae289e5015ec49757018a6a080575334f4
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-20 01:57:05 -0400

    Tested with hardware

--- vhdl-project-uart.srcs/sources_1/new/uart_system.vhd
+++ vhdl-project-uart.srcs/sources_1/new/uart_system.vhd
@@ -5,7 +5,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity uart_system is
     port (sysclk: in std_logic;
           btn: in std_logic_vector(0 downto 0);
-          jc: out std_logic_vector(0 downto 0));
+          led: out std_logic_vector(0 downto 0));
 end uart_system;
 
 architecture Behavioral of uart_system is
@@ -15,9 +15,29 @@ architecture Behavioral of uart_system is
             tick: out std_logic  
         ); 
     end component;
+    
+    signal temp : integer := 0;
+    signal tick : std_logic := '0';
 
 begin
 
-    UUT : baud_rate_generator port map (clk => sysclk, reset => btn(0), tick => jc(0));
+    UUT : baud_rate_generator port map (clk => sysclk, reset => btn(0), tick => tick);
+    
+    process(tick)
+    begin
+    
+        if (tick'event and tick = '1') then
+            temp <= temp + 1;
+        end if;
+        
+        if temp >= 1000000 then
+        
+            led(0) <= '1';
+            
+        else
+            led(0) <= '0';
+        end if;
+    
+    end process;
 
 end Behavioral;

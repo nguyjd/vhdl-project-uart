11daf27a76e307efbf88b4f0bed94c30e602b66d
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-20 10:48:02 -0400

    Created two files

---
+++ vhdl-project-uart.srcs/sources_1/new/ram.vhd
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity ram is
+--  Port ( );
+end ram;
+
+architecture Behavioral of ram is
+
+begin
+
+
+end Behavioral;
---
+++ vhdl-project-uart.srcs/sources_1/new/ram_controller.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+
+entity ram_controller is
+--  Port ( );
+end ram_controller;
+
+architecture Behavioral of ram_controller is
+
+begin
+
+
+end Behavioral;

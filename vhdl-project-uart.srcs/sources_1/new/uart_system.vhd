f48ec03655dd81777bef728b32f26cb4a1eeae5b
Jonathon Nguyen <nguyjd@gmail.com>
2022-04-25 22:44:03 -0400

    Connected the three modules

--- vhdl-project-uart.srcs/sources_1/new/uart_system.vhd
+++ vhdl-project-uart.srcs/sources_1/new/uart_system.vhd
@@ -4,9 +4,10 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity uart_system is
     generic(D_WIDTH: natural := 8);
-    port (sysclk: in std_logic;
-          btn: in std_logic_vector(0 downto 0);
-          led: out std_logic_vector(0 downto 0));
+    port (clk, reset, rx, tx_start: in std_logic;
+          rx_done, tx_done, tx: out std_logic;
+          rx_data: out std_logic_vector(D_WIDTH - 1 downto 0);
+          tx_data: in std_logic_vector(D_WIDTH - 1 downto 0));
 end uart_system;
 
 architecture Behavioral of uart_system is
@@ -24,13 +25,6 @@ architecture Behavioral of uart_system is
         ); 
     end component;
     
-    component fifo_buffer is
-        Port (  clk, reset, write_req, read_req : in std_logic;
-                data_out : out std_logic_vector (D_WIDTH - 1 downto 0);
-                data_in : in std_logic_vector (D_WIDTH - 1 downto 0);
-                full, empty : out std_logic);
-    end component;
-    
     component uart_transmitter is
         port(clk, reset: in std_logic;
          tx_start: in std_logic;
@@ -40,31 +34,25 @@ architecture Behavioral of uart_system is
          tx: out std_logic);
     end component;
     
-    signal rx, tick, rx_done_tick, tx_start, tx_done_tick: std_logic := '0';
-    
-    signal clk, reset, read_req, full, empty : std_logic := '0';
-    signal data_out, rx_data, data_in: std_logic_vector (D_WIDTH - 1 downto 0) := (others => '0');
+    signal tick: std_logic := '0';
 
 begin
 
-    baud : baud_rate_generator port map (clk => sysclk, reset => reset, tick => tick);
+    baud : baud_rate_generator port map (clk => clk, reset => reset, tick => tick);
     
-    uart_rx : uart_receiver port map (clk => sysclk, 
+    uart_rx : uart_receiver port map (clk => clk, 
                                       rx => rx,
                                       s_tick => tick,
                                       reset => reset,
-                                      rx_done_tick => rx_done_tick,
+                                      rx_done_tick => rx_done,
                                       data_out => rx_data);
-     
-    rx_fifo : fifo_buffer port map (clk => sysclk, 
-                                    reset => reset,
-                                    write_req => rx_done_tick,
-                                    read_req => read_req,
-                                    data_out => data_out,
-                                    data_in => rx_data,
-                                    full => full,
-                                    empty => empty);
-                                    
-    reset <= btn(0);
+    
+    uart_tx : uart_transmitter port map (clk => clk,
+                                         tx_start => tx_start,
+                                         tx => tx,
+                                         s_tick => tick,
+                                         reset => reset,
+                                         data_in => tx_data,
+                                         tx_done_tick => tx_done);
 
 end Behavioral;
